3cd0f93e260f8be1ebed57e3771e4cc1e00c8459
VSFe <klm03025@gmail.com>
2022-03-11 20:29:24 +0900

    [ADD] Strategy Pattern On Java

---
+++ 01/KBC/head-first-design-pattern/src/test/java/com/vsfe/headfirstdesignpattern/HeadFirstDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsfe.headfirstdesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadFirstDesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
